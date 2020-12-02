dbf45d41791dfd11a3e9d37b7b53a0ec452c19f2
mingeon park <devmin7985@gmail.com>
2020-12-02 18:03:49 +0900

    fixed basic code and add string fomating

--- app/src/main/java/com/example/deitem_login/LoginActivity.java
+++ app/src/main/java/com/example/deitem_login/LoginActivity.java
@@ -7,6 +7,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.RequestQueue;
@@ -16,74 +22,58 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText et_id, et_pass; //input text
-    private Button btn_login, btn_register; //button event
+    private EditText email, password; //input text
+    private Button btn_login, btn_sign_up; //button event
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { //main
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_login );
 
-        et_id = findViewById( R.id.et_id ); //id object
-        et_pass = findViewById( R.id.et_pass ); //pw object
+        email = findViewById( R.id.login_emain ); //id object
+        password = findViewById( R.id.login_password ); //pw object
+        btn_login = findViewById( R.id.btn_login );
+        btn_sign_up = findViewById( R.id.btn_sign_up ); //btn object
 
-        btn_register = findViewById( R.id.btn_register ); //btn object
-        btn_register.setOnClickListener( new View.OnClickListener() {
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btn_sign_up.setOnClickListener( new View.OnClickListener() { // sign_up
             @Override
-            public void onClick(View view) { //
-                Intent intent = new Intent( com.example.deitem_login.LoginActivity.this, com.example.deitem_login.RegisterActivity.class );
+            public void onClick(View view) { //change screen register intent
+                Intent intent = new Intent( com.example.deitem_login.LoginActivity.this, com.example.deitem_login.SignUpActivity.class );
                 startActivity( intent );
             }
         });
 
-
-        btn_login = findViewById( R.id.btn_login );
-        btn_login.setOnClickListener( new View.OnClickListener() {
+        btn_login.setOnClickListener( new View.OnClickListener() { // Login
             @Override
             public void onClick(View view) {
-                String userID = et_id.getText().toString();
-                String userPass = et_pass.getText().toString();
+                if (password.length()<8) { //8이하의 비밀번호
+                    Toast.makeText(LoginActivity.this, "비밀번호는 8자 이상만 가능합니다.", Toast.LENGTH_SHORT).show();
+                    password.getText().clear();
+                    return;
+                }
+                String userID = email.getText().toString().trim();
+                String userPwd = password.getText().toString().trim();
 
-                Response.Listener<String> responseListener = new Response.Listener<String>() {
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonObject = new JSONObject( response );
-                            boolean success = jsonObject.getBoolean( "success" ); //?
-
-                            if(success) { //로그인 성공시
-
-                                String userID = jsonObject.getString( "userID" );
-                                String userPass = jsonObject.getString( "userPassword" );
-                                String userName = jsonObject.getString( "userName" );
-                                String userAge = jsonObject.getString( "userAge" );
-
-                                Toast.makeText( getApplicationContext(), "로그인 성공", Toast.LENGTH_SHORT ).show();
-                                Intent intent = new Intent( com.example.deitem_login.LoginActivity.this, MainActivity.class ); //intent object - > ohter activity call
-
-                                intent.putExtra( "userID", userID ); //?
-                                intent.putExtra( "userPass", userPass );
-                                intent.putExtra( "userName", userName );
-                                intent.putExtra( "userAge", userAge );
-
-                                startActivity( intent );
-
-                            } else {//로그인 실패시
-                                Toast.makeText( getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT ).show();
-                                return;
+                firebaseAuth.signInWithEmailAndPassword(userID, userPwd)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, "로그인 성공!.", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "아이디 혹은 비밀번호가 틀렸습니다.", Toast.LENGTH_SHORT).show();
+                                    password.getText().clear();
+                                }
                             }
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                };
-
-                com.example.deitem_login.LoginRequest loginRequest = new com.example.deitem_login.LoginRequest( userID, userPass, responseListener ); //user create object
-                RequestQueue queue = Volley.newRequestQueue( com.example.deitem_login.LoginActivity.this );
-                queue.add( loginRequest );
-
+                });
             }
         });
     }
---
+++ app/src/main/java/com/example/deitem_login/signUpActivity.java
@@ -0,0 +1,87 @@
+package com.example.deitem_login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firestore.v1.FirestoreGrpc;
+
+import java.util.regex.Pattern;
+
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText sign_email , sign_password, sign_name, sign_phone;
+    private Button singup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        sign_email = (EditText) findViewById(R.id.sign_email);
+        sign_password = (EditText) findViewById(R.id.sign_pwd);
+        sign_name = (EditText) findViewById(R.id.sign_name);
+        sign_phone = (EditText) findViewById(R.id.sign_phone);
+        singup = (Button) findViewById(R.id.sign_up);
+
+        FirebaseAuth firebaseAuth;
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        singup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (sign_password.length()<8) { //8이하의 비밀번호
+                    Toast.makeText(SignUpActivity.this, "비밀번호는 8자 이상만 가능합니다.", Toast.LENGTH_SHORT).show();
+                    sign_password.getText().clear();
+                    return;
+                }
+
+                if (!(Pattern.matches("^[가-힣]{2,4}$", sign_name.getText().toString().trim()))) {
+                    Toast.makeText(SignUpActivity.this, "이름을 다시 확인해 주십시오.", Toast.LENGTH_SHORT).show();
+                    sign_name.getText().clear();
+                    return;
+                }
+
+                if (!(Pattern.matches("^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$", sign_phone.getText().toString().trim()))) {
+                    Toast.makeText(SignUpActivity.this, "전화번호가 맞지않습니다.", Toast.LENGTH_SHORT).show();
+                    sign_phone.getText().clear();
+                    return;
+                }
+
+                String email = sign_email.getText().toString().trim();
+                String password = sign_password.getText().toString().trim();
+                String name = sign_name.getText().toString().trim();
+
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                                    Toast.makeText(SignUpActivity.this, "회원가입에 완료했습니다.", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(SignUpActivity.this, "이메일형식이 잘못되었습니다. 다시 입력해주세요.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+    }
+}
